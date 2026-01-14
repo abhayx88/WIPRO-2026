f73f3f26759f55615031d59e9c6600352b5edb27
abhayx88 <97593611+abhayx88@users.noreply.github.com>
2026-01-14 10:30:37 +0530

    tc_functions.py

--- tc_functions.py
+++ tc_functions.py
@@ -6,3 +6,16 @@ def sub(a,b):
 
 add(10,20)
 print(sub(100,20))
+def hello(greeting="Hello",name="world"):
+    print('%s,%s'%(greeting,name))
+hello()
+hello('Greetings','Deepa')
+def print_param(*params):
+    print(params)
+print_param('Testing')
+print_param(1,2,3,4)
+def print_param1(**params):
+    print(params)
+
+print_param1(x=1,y=2,z=3)
+
