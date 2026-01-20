99677b328dffe1c2bfc10e125591033b9c8b603a
abhayx88 <97593611+abhayx88@users.noreply.github.com>
2026-01-20 10:54:21 +0530

    Update TC_Match.py

--- DAY 6/TC_Match.py
+++ DAY 6/TC_Match.py
@@ -14,5 +14,12 @@ print(searchresult.end())
 email="admin@gmail.com"
 if re.match(r"[a-zA-Z]+@",email):
     print("Valid Start")
-else:
-    print("no Valid Start")
+
+result2=re.fullmatch(r"\d{10}","1234567898")
+print(result2)
+
+
+print(re.findall(r"\d+","price 50 and 100 and 200"))
+
+for n in re.finditer(r"\d+","A1, B33, C444"):
+    print(n.group(),n.start(),n.end())
